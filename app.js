21cac62f2eaa54c9554a8a69ba519a8b88030b2b
trymiz <trymmagnusdal@gmail.com>
2024-01-16 15:18:43 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.blue('What ') + chalk.red('are ') + chalk.green('spiders ') + chalk.yellow('really ') + chalk.magenta('good ') + chalk.cyan('at? ') 
++ chalk.redBright('surfing ') + chalk.white('the ') + chalk.greenBright('web '));
